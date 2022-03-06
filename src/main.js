4b499d353c043da8008428af8eb31fe458dafeb0
Carolina Nardini <caronardini@gmail.com>
2022-03-06 19:24:55 -0300

    Entrega 1

--- src/main.js
+++ src/main.js
@@ -1,14 +1,40 @@
-let texto = prompt("Ingresa una palabra");
-let acumulador="";
+// DESAFIO 1
+// let texto = prompt("Ingresa una palabra");
+// let acumulador="";
 
-while(texto!="esc"){
-    alert(texto);
+// while(texto!="esc"){
+//     alert(texto);
 
-    acumulador +=texto + " ";
+//     acumulador +=texto + " ";
 
-    texto=prompt("Ingresa otra palabra");
+//     texto=prompt("Ingresa otra palabra");
+// }
+
+
+// alert(acumulador);
+// document.write(acumulador);
+
+
+// FUNCIONES
+
+function validarStock(stockProducto,cantidadPedida){
+    if(stockProducto>cantidadPedida){
+        return true;
+    }
+    else{
+       return false;
+    }
 }
 
+function agregarAlCarrito(nombreProducto,precioProducto=0,stockProducto,cantidadPedida){
+    const tieneStock=validarStock(stockProducto,cantidadPedida);
 
-alert(acumulador);
-document.write(acumulador);
+    if(tieneStock){
+        console.log(`agregaste al carrito el ${nombreProducto} a $ ${precioProducto}`)
+    }    
+    else{
+        console.log("No tenemos más producto");
+    }
+
+}
+
