6ccbc0033dd9a736192bef2da6a0d35615a375dc
Carolina Nardini <caronardini@gmail.com>
2022-05-04 19:37:33 -0300

    entrega v7

--- src/detalle.js
+++ src/detalle.js
@@ -11,7 +11,7 @@ document.getElementById("cardContainer").innerHTML = `
         </div>
         <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
         <div class="d-flex">
-            <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
+            <input class="form-control text-center me-3" id="cantidad-${productoAVer.id}" type="num" value="1" style="max-width: 3rem" />
             <button onclick="agregarAlCarrito(${productoAVer.id})" class="btn btn-outline-dark flex-shrink-0" type="button">
                 <i class="bi-cart-fill me-1"></i>
                 Agregar al carrito
--- src/main.js
+++ src/main.js
@@ -117,7 +117,7 @@ function tomarValor(){
 
 
 function eliminarDelCarrito (id){
-    let carrito =JSON.parse(localStorage.getItem("carrito)"));
+    let carrito =JSON.parse(localStorage.getItem("carrito"));
     let nuevoCarrito =carrito.filter(e=>e.id!=id);
     localStorage.setItem("carrito", JSON.stringify(nuevoCarrito));
     
